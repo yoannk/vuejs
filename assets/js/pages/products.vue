833b96ab355b03f34c639e3c75554c59b77f7f26
Yoann Kergall <yoann.kergall@gmail.com>
2024-01-31 23:49:52 +0100

    feat(chap-07): child-component

--- assets/js/pages/products.vue
+++ assets/js/pages/products.vue
@@ -42,9 +42,7 @@
                     </div>
                 </div>
                 <div class="row">
-                    <span class="p-3">
-                        {{ legend }}
-                    </span>
+                    <legend-component />
                 </div>
             </div>
         </div>
@@ -52,8 +50,13 @@
 </template>
 
 <script>
+import LegendComponent from '../components/legend';
+
 export default {
     name: 'Products',
+    components: {
+        LegendComponent,
+    },
     data() {
         return {
             legend: 'Shipping takes 10-12 weeks, and products probably won\'t work',
